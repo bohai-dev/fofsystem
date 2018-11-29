afe889aeb3ed1c035065cbabdc532bfac3d4bbce
caoxingyu <caoxingyu2016@qq.com>
2018-11-29 09:15:30 +0800

    first commit

---
+++ src/test/java/com/bohai/fofsystem/FofsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bohai.fofsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FofsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
